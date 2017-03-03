bfc46ffa1a179ec82d640e4a17c04b142bc40660
hrishikesh.deodhar <hrishikesh.deodhar@hcl.com>
2017-03-03 14:55:24 +0530

    Adding AWS config for Public DNS usage

--- src/main/java/com/ewolff/microservice/order/OrderApp.java
+++ src/main/java/com/ewolff/microservice/order/OrderApp.java
@@ -1,10 +1,17 @@
 package com.ewolff.microservice.order;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,8 +19,29 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 @RibbonClient("order")
 public class OrderApp {
 
+	@Value("${server.port}") 
+	private int port;
+	
+	@Value("${spring.application.name}")
+	private String name;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApp.class, args);
 	}
 
+	@Bean
+	@Profile(value="aws")
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+	    EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+	    AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild(name);
+	    config.setDataCenterInfo(info);
+	    info.getMetadata()
+	            .put(AmazonInfo.MetaDataKey.publicHostname.getName(), info.get(AmazonInfo.MetaDataKey.publicHostname));
+	    config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	    config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	    config.setNonSecurePort(port);
+	    return config;
+	}
+
+	
 }
